b2a5293e8abbb9b76cce5bd2d4ebb52c23a82061
naari3 <naari.named@gmail.com>
2021-11-13 17:47:42 +0900

    Pass test case 01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn derive(_input: TokenStream) -> TokenStream {
+    let expanded = quote! {};
+    TokenStream::from(expanded)
 }
